f5d1e7e9d177196b1a016a1b4914d9325f543945
Luanna Virginia <86988841+luandradevirginiamelo@users.noreply.github.com>
2023-04-25 12:43:04 +0100

    Update onHitwall() method

--- Robot1236.java
+++ Robot1236.java
@@ -4,6 +4,8 @@ import robocode.*;
 import robocode.util.Utils;
 import java.util.Random;
 
+import javafx.scene.control.skin.TextInputControlSkin.Direction;
+
 import java.awt.Color;
 import java.awt.geom.Point2D;
 
@@ -19,28 +21,42 @@ public class Robot1236 extends AdvancedRobot {
 
 	public void run() {
 		// Set colors
-    	setColors(Color.yellow, Color.green, Color.red);
+		setColors(Color.yellow, Color.green, Color.red);
 
-    	while (true) {
-		moveRandomly();
+		while (true) {
+			moveRandomly();
 		}
 	}
-	
+
 	private void moveRandomly() {
 		ahead(moveDistance * (random.nextDouble() * 2 - 1));
 		turnRight(90 * (random.nextDouble() * 2 - 1));
 		execute();
 	}
-	
+
+	// ### Luanna ###
 	public void onHitWall(HitWallEvent e) {
-		//Reverse direction upon hitting a wall
-		moveDistance *= -1;
+		// Will random generate an angle between 45 and 135 degrees
+		double turnAngle = Math.toRadians((Math.random() * 90) + 45);
+		// Wiçç random generate a distance to move foward betweem 50 and 150 degrees
+		double distance = (Math.random() * 100) + 50;
+		// If math random is less than 0.5 will randomly turn for right or left
+		if (Math.random() < 0.5) {
+			// Turn right based on the random turnAngle
+			turnRightRadians(turnAngle);
+		} else {
+			// Turn çeft based on the random turnAngle
+			turnLeftRadians(turnAngle);
+		}
+		// Go ahead a random distance
+		ahead(distance);
 	}
-	
+	// ### Luanna ###
+
 	public void onScannedRobot(ScannedRobotEvent e) {
-		//Turn towards scanned robot
+		// Turn towards scanned robot
 		turnRight(e.getBearing());
-		//Fire at scanned robot
+		// Fire at scanned robot
 		fire(1);
 	}
-}	
+}
