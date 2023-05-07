ba2fe42cc572a7f0348c0cc2858eac758b1b1e20
Luanna Virginia <86988841+luandradevirginiamelo@users.noreply.github.com>
2023-05-07 23:18:12 +0100

    Update Robot1236.java
    improvements

--- Robot1236.java
+++ Robot1236.java
@@ -4,15 +4,13 @@ import robocode.*;
 import robocode.util.Utils;
 import java.util.Random;
 
-import javafx.scene.control.skin.TextInputControlSkin.Direction;
-
 import java.awt.Color;
 import java.awt.geom.Point2D;
 
 // API help : https://robocode.sourceforge.io/docs/robocode/robocode/Robot.html
 
 /**
- * Robot1236 - a robot by (your name here)
+ * Robot1236 - a robot by (Luanna Virginia)
  */
 public class Robot1236 extends AdvancedRobot {
 
@@ -29,33 +27,32 @@ public class Robot1236 extends AdvancedRobot {
 	}
 
 	private void moveRandomly() {
-		ahead(moveDistance * (random.nextDouble() * 2 - 1));
-		turnRight(90 * (random.nextDouble() * 2 - 1));
+		setAhead(moveDistance * (random.nextDouble() * 2 - 1));
+		setTurnRight(90 * (random.nextDouble() * 2 - 1));
 		execute();
 	}
 
-	// ### Luanna ###
 	public void onHitWall(HitWallEvent e) {
-		// Will random generate an angle between 45 and 135 degrees
+		// Will randomly generate an angle between 45 and 135 degrees
 		double turnAngle = Math.toRadians((Math.random() * 90) + 45);
-		// Wiçç random generate a distance to move foward betweem 50 and 150 degrees
+		// Will randomly generate a distance to move forward between 50 and 150 units
 		double distance = (Math.random() * 100) + 50;
 		// If math random is less than 0.5 will randomly turn for right or left
 		if (Math.random() < 0.5) {
 			// Turn right based on the random turnAngle
-			turnRightRadians(turnAngle);
+			setTurnRightRadians(turnAngle);
 		} else {
-			// Turn çeft based on the random turnAngle
-			turnLeftRadians(turnAngle);
+			// Turn left based on the random turnAngle
+			setTurnLeftRadians(turnAngle);
 		}
 		// Go ahead a random distance
-		ahead(distance);
+		setAhead(distance);
+		execute();
 	}
-	// ### Luanna ###
 
 	public void onScannedRobot(ScannedRobotEvent e) {
 		// Turn towards scanned robot
-		turnRight(e.getBearing());
+		setTurnRight(e.getBearing());
 		// Fire at scanned robot
 		fire(1);
 	}
